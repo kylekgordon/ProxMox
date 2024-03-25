c6aa1e9f1193e8b5ab43d302c105c4d6742d7bd8
Mr.Gordon <58670552+kylekgordon@users.noreply.github.com>
2024-03-24 21:29:58 -0500

    Update plex.sh

--- plex.sh
+++ plex.sh
@@ -37,7 +37,7 @@ function default_settings() {
   CORE_COUNT="$var_cpu"
   RAM_SIZE="$var_ram"
   BRG="vmbr0"
-  NET="192.168.1.106//24"
+  NET="192.168.1.106"
   GATE=""
   APT_CACHER=""
   APT_CACHER_IP=""
